372b17477a515dfe34e436e61716c2ce1854c105
jhonnierandrey <jhonnierandrey@outlook.com>
2023-09-24 13:20:24 -0300

    0924 - finishing project

---
+++ Shared/Shapes/ChatBubble.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBubble.swift
+//  iChatGPT
+//
+//  Created by Jhonnier Zapata on 9/22/23.
+//
+
+import SwiftUI
+
+struct ChatBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBubble()
+}
